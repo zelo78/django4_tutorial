9c92066bb21f41142fdf4ce3ba46ead74ae1af51
Oleg <zelo78@yandex.ru>
2022-02-09 02:08:27 +0300

    Add TODO

--- polls/admin.py
+++ polls/admin.py
@@ -2,4 +2,6 @@ from django.contrib import admin
 
 from .models import Question
 
+# TODO add custom QuestionAdmin for inline add, edit and create Choices of Question
+
 admin.site.register(Question)
